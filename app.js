52ba9d78c1b77b108b8821f80a90dbde04ca0bd6
Stein Ramirez <steinvzla@gmail.com>
2022-12-07 13:17:11 -0500

    changes

---
+++ app.js
@@ -0,0 +1,32 @@
+var btnServc = document.getElementById('buttonService');
+var stgbtn = document.getElementById('stgbtn');
+var dsgnbtn = document.getElementById('dsgnbtn');
+var brnbtn = document.getElementById('brnbtn');
+var worksbtn = document.getElementById('worksbtn');
+var prjctybtn = document.getElementById('prjctybtn');
+var typebtn = document.getElementById('typebtn');
+var symbolbtn = document.getElementById('symbolbtn');
+var illustratorbtn = document.getElementById('illustratorbtn');
+var digitalbtn = document.getElementById('digitalbtn');
+var webbtn = document.getElementById('webbtn');
+
+
+var svc = document.getElementById('Services');
+var strg = document.getElementById('Strategy');
+var design = document.getElementById('Design');
+var branding = document.getElementById('Branding');
+var works = document.getElementById('Works');
+var project = document.getElementById('Project');
+var typeJournal = document.getElementById('TypeJournal');
+var symbols = document.getElementById('Symbols');
+var Illustration = document.getElementById('Illustration');
+var digitalM = document.getElementById('DigitalM');
+var webUi = document.getElementById('WebUi');
+var contact = document.getElementById('Contact');
+var blog = document.getElementById('Blog');
+
+btnServc.addEventListener('click', () => {svc.classList.toggle('sliderActive');
+svc.classList.toggle ('slider');});
+
+
+
